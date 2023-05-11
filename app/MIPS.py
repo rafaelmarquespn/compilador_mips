22385f17b3c0cfa6c78b1197c26d829ee15a9b12
rafaelmarquespn <rafaelmarquespn>
2023-05-11 15:13:11 -0300

    atualizações

--- app/MIPS.py
+++ app/MIPS.py
@@ -1,14 +1,86 @@
-class translator():
-    pass
+class Registers():
+    def __init__(self) -> None:
+        pass
+        
+    def zero(self, registers:str):
+        return "00000"
+    def at(self, registers:str):
+        return "00001"
+    def v0(self, registers:str):
+        return "00010"
+    def v1(self, registers:str):
+        return "00011"
+    def a0(self, registers:str):
+        return "00100"
+    def a1(self, registers:str):
+        return "00101"
+    def a2(self, registers:str):
+        return "00110"
+    def a3(self, registers:str):
+        return "00111"
+    def t0(self, registers:str):
+        return "01000"
+    def t1(self, registers:str):
+        return "01001"
+    def t2(self, registers:str):
+        return "01010"
+    def t3(self, registers:str):
+        return "01011"
+    def t4(self, registers:str):
+        return "01100"
+    def t5(self, registers:str):
+        return "01101"
+    def t6(self, registers:str):
+        return "01110"
+    def t7(self, registers:str):
+        return "01111"
+    def s0(self, registers:str):
+        return "10000"
+    def s1(self, registers:str):
+        return "10001"
+    def s2(self, registers:str):
+        return "10010"
+    def s3(self, registers:str):
+        return "10011"
+    def s4(self, registers:str):
+        return "10100"
+    def s5(self, registers:str):
+        return "10101"
+    def s6(self, registers:str):
+        return "10110"
+    def s7(self, registers:str):
+        return "10111"
+    def t8(self, registers:str):
+        return "11000"
+    def t9(self, registers:str):
+        return "11001"
+    def k0(self, registers:str):
+        return "11010"
+    def k1(self, registers:str):
+        return "11011"
+    def gp(self, registers:str):
+        return "11100"
+    def sp(self, registers:str):
+        return "11101"
+    def fp(self, registers:str):
+        return "11110"
+    def ra(self, registers:str):
+        return "11111"
+    
+
+class Translator(Registers):
+    def __init__(self) -> None:
+        pass
+    def add_d(self, registers:str):
+        return 100100
 
 
-
-def leitura_do_arquivo(path:str) -> list:
+def load_file(path:str) -> list:
     with open(path, 'r', encoding="utf-8") as arq:
         asm = arq.readlines()
     return asm
 
-def separar_data_text(asm:list)->list:
+def split_data_text(asm:list)->list:
     data = list()
     text = list()
     data_area = None
@@ -28,13 +100,15 @@ def separar_data_text(asm:list)->list:
 
     return data, text
 
-def traduzir_text(text:list)->list:
-    lista = list()
+def translate_text(text:list)->list:
+    translated = list()
     for i in text:
         match i:
             case 'add.d $f0, $f2':
-                k = add.d()
-                lista.append(k)
+                registers = i.split(' ')[1]
+                translator = Translator()
+                translator.add_d(registers)
+                translated.append(translator)
 
 
 
@@ -42,8 +116,8 @@ def traduzir_text(text:list)->list:
 
 
 
-asm = leitura_do_arquivo("app\\archives\\example_saida.asm")
-data, text = separar_data_text(asm)
+asm = load_file("app\\archives\\example_saida.asm")
+data, text = split_data_text(asm)
 print(data)
 print()
 print(text)
--- app/teste.py
+++ app/teste.py
@@ -1,10 +1,74 @@
-with open('app\\archives\\exemplos_in.txt', 'r') as f :
-    i = f.readlines()
-    i = [m.strip() for m in i]
-    lista = i.sort()
-    pass
-    with open('app\\archives\\exemplos_in.txt', 'w') as f :
-        for n in i:
-            f.write(n +'\n')
-        
+def ordem_alfabetica():
+    with open('app\\archives\\exemplos_in.txt', 'r') as f :
+        i = f.readlines()
+        i = [m.strip() for m in i]
+        lista = i.sort()
+        pass
+        with open('app\\archives\\exemplos_in.txt', 'w') as f :
+            for n in i:
+                f.write(n +'\n')
 
+
+def separar_linha():
+    lista = []
+    with open('compilador_mips\\app\\archives\\exemplos_in.txt', 'r') as arq :
+        i = arq.readlines()
+        i = [m.strip() for m in i]
+        for n in i:
+            if '/' in n:
+                instruções, registradores = n.split(' ', 1)
+                instruções = instruções.split('/')
+                for i in instruções:
+                    lista.append(i + ' ' + registradores)
+            else:
+                lista.append(n)
+
+    with open('compilador_mips\\app\\archives\\exemplos_in.txt', 'w') as arq :
+        for n in lista:
+            arq.write(n +'\n')
+
+
+def match_case_automatico():
+    lista = []
+    with open('compilador_mips\\app\\archives\\exemplos_in.txt', 'r') as arq :
+        i = arq.readlines()
+        i = [m.strip() for m in i]
+        for n in i:
+            j = n.replace('.','_')
+            instruções, registradores = n.split(' ', 1)
+                # case 'add.d $f0, $f2':#
+                # registers = i.split(' ')[1]
+                # translator = Translator()
+                # traduction = translator.add_d(registers)
+                # translated.append(translator)
+            lista.append("case " + f"'{n}'" + ":" + "\n" + f'\tregisters = i.split(' f')[1]\n\ttranslator = Translator()\n\ttraduction = translator.{j}({registradores})\n\ttranslated.append(traduction) ' + "\n")
+
+    with open('compilador_mips\\app\\archives\\exemplos_match.txt', 'w') as arq :
+        for n in lista:
+            arq.write(n +'\n')
+
+#match_case_automatico()
+
+def metodos_automatico():
+    with open('compilador_mips\\app\\archives\\exemplos_in.txt', 'r') as arq :
+        i = arq.readlines()
+        i = [m.strip() for m in i]
+        lista = []
+        for n in i:
+            j = n.replace('.','_')
+            lista.append(f'def {j}(self, registers:str):\n\tpass\n')    
+
+
+def instrucoes():
+    with open('compilador_mips\\app\\archives\\entradas_validas.txt', 'r') as arq :
+        i = arq.readlines()
+        i = [m.strip() for m in i]
+        lista = []
+        for n in i:
+            j = n.split(' ', 1)[0]
+            lista.append(f'{j}')    
+    with open('compilador_mips\\app\\archives\\instrucoes.txt', 'w') as arq :
+        for n in lista:
+            arq.write(n +'\n')
+
+instrucoes()
